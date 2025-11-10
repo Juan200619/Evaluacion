22a007cb8ded26dc27a67b9a71524daf48cd7e3f
Juankla <140849728+Juankla@users.noreply.github.com>
2025-11-10 11:25:04 -0500

    } let n1= parseInt ("Ingrese el primer numero")
    Juan Cadena Multiplicacion

---
+++ multiplicaicon.js
@@ -0,0 +1,12 @@
+function multiplicacion (n1,n2){
+let resultado = n1 * n2;
+return resultado
+
+
+}
+let n1= parseIntpromt("Ingrese el primer numero");
+let n2= parseIntpromt("Ingrese el segundo numero");
+let solucion;
+solucion (n1,n2);
+
+console.log (multiplicacion (n1,n2));
