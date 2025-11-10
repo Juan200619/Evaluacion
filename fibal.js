86922f5da79492fe2340d29dbae7ffdff0e965c4
Juan200619 <juancortazar.2006@gmail.com>
2025-11-10 11:35:27 -0500

    Add files via upload

---
+++ fibal.js
@@ -0,0 +1,14 @@
+//elaborar un adgoritmo que muestre cual es el promedio de las eedades de los prendices de la ficha y cuantos osn mayores de edad.
+
+
+  FileSystemWritableFileStream,JSONpromedio {
+
+  
+  float i = < 18 = 18
+  edades i = < 17 = 
+  }
+
+// diga si es mayor de edad 
+
+
+//cuantos son mayores de edad
