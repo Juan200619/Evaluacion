bf360a7c69498f1f00b29fd5dd109009bf632864
Julian09192 <julianvesga135@gmail.com>
2025-11-10 11:23:10 -0500

    Multiplicación

---
+++ Multiplicación.js
@@ -0,0 +1,7 @@
+function multi(a,b,c) {
+    c=a*b;
+    return c;
+}
+let j = Int(Number(prompt("digite un numero")));
+let i = Int(Number(prompt("digite un numero")));
+let l = console.log(multi);
