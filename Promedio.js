1583cd9ec57b29ac9baa398ef0797ade7802faf0
Julian09192 <julianvesga135@gmail.com>
2025-11-10 11:36:05 -0500

    Edades promedio

---
+++ Promedio.js
@@ -0,0 +1,15 @@
+function promedioEdad(edad,sumDatos,promedio,canaprendices) {
+    
+    for (let i= 0; i< canaprendices; i++) {
+        if (i<18) {
+            
+        } else {
+            
+        }
+        promedio=sumdatos+edad/2;
+
+    }    
+        return promedio;
+
+}
+ let a=Int
